5fde1267bcb5fb1aeaff5f33ced9d9e60857647e
chaitanya197803 <cgarg9432@gmail.com>
2025-01-22 09:46:04 +0530

    Ham Button

--- app/page.js
+++ app/page.js
@@ -5,7 +5,116 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      Before the telephone, verbal greetings often involved a time of day, such as "good morning". When the telephone began connecting people in different time zones, greetings without time gained popularity.[11]
+
+      Thomas Edison is credited with popularizing hullo as a telephone greeting. In previous decades, hullo had been used as an exclamation of surprise (used early on by Charles Dickens in 1850)[12] and halloo was shouted at ferry boat operators by people who wanted to catch a ride.[13] According to one account, halloo was the first word Edison yelled into his strip phonograph when he discovered recorded sound in 1877.[12] Shortly after Alexander Graham Bell invented the telephone, he answered calls by saying "ahoy ahoy", borrowing the term used on ships.[13][14] There is no evidence the greeting caught on.[13] Edison suggested Hello! on August 15, 1877 in a letter to the president of Pittsburgh's Central District and Printing Telegraph Company, T. B. A. David:
+
+      Friend David, I do not think we shall need a call bell as Hello! can be heard 10 to 20 feet away. What you think? Edison – P.S. first cost of sender & receiver to manufacture is only $7.00.[12]
+
+      The first name tags to include Hello may have 1880 at Niagara Falls, which was the site of the first telephone operators convention. By 1889, central telephone exchange operators were known as "hello-girls" because of the association between the greeting and the telephone.[14][15]
+
+      A 1918 novel uses the spelling "Halloa" in the context of telephone conversations.[16]
+
+      Hullo, hallo, and other spellings
+      "Hallo" redirects here. For other uses, see Hallo (disambiguation).
+      Hello might be derived from an older spelling variant, hullo, which the American Merriam-Webster dictionary describes as a "chiefly British variant of hello",[17] and which was originally used as an exclamation to call attention, an expression of surprise, or a greeting. Hullo is found in publications as early as 1803.[18] The word hullo is still in use, with the meaning hello.[19][20][21][22]
+
+      Hello is alternatively thought to come from the word hallo (1840) via hollo (also holla, holloa, halloo, halloa).[23] The definition of hollo is to shout or an exclamation originally shouted in a hunt when the quarry was spotted:[24][25]
+
+      If I fly, Marcius,/Halloo me like a hare.
+
+      — Coriolanus (I.viii.7), William Shakespeare
+      Fowler's has it that "hallo" is first recorded "as a shout to call attention" in 1864.[26] It is used by Samuel Taylor Coleridge's famous poem The Rime of the Ancient Mariner written in 1798:
+
+      And the good south wind still blew behind,
+      But no sweet bird did follow,
+      Nor any day for food or play
+
+      Came to the mariners' hollo!
+
+      In many Germanic languages, including German, Danish, Norwegian, Dutch and Afrikaans, "hallo" directly translates into English as "hello". In the case of Dutch, it was used as early as 1797 in a letter from Willem Bilderdijk to his sister-in-law as a remark of astonishment.[27]
+
+      Webster's dictionary from 1913 traces the etymology of holloa to the Old English halow and suggests: "Perhaps from ah + lo; compare Anglo Saxon ealā".
+
+      According to the American Heritage Dictionary, hallo is a modification of the obsolete holla (stop!), perhaps from Old French hola (ho, ho! + la, there, from Latin illac, that way).[28]
+
+      "Hello, World" computer program
+      Main article: "Hello, World!" program
+      Students learning a new computer programming language will often begin by writing a "Hello, World!" program, which does nothing but issue the message "Hello, World!" to the user (such as by displaying it on a screen). It has been used since the earliest programs, and in many computer languages. This tradition was further popularised after being printed in an introductory chapter of the book The C Programming Language by Kernighan & Ritchie.[29] The book had reused an example taken from a 1974 memo by Brian Kernighan at Bell Laboratories.[30]
+
+      Before the telephone, verbal greetings often involved a time of day, such as "good morning". When the telephone began connecting people in different time zones, greetings without time gained popularity.[11]
+
+      Thomas Edison is credited with popularizing hullo as a telephone greeting. In previous decades, hullo had been used as an exclamation of surprise (used early on by Charles Dickens in 1850)[12] and halloo was shouted at ferry boat operators by people who wanted to catch a ride.[13] According to one account, halloo was the first word Edison yelled into his strip phonograph when he discovered recorded sound in 1877.[12] Shortly after Alexander Graham Bell invented the telephone, he answered calls by saying "ahoy ahoy", borrowing the term used on ships.[13][14] There is no evidence the greeting caught on.[13] Edison suggested Hello! on August 15, 1877 in a letter to the president of Pittsburgh's Central District and Printing Telegraph Company, T. B. A. David:
+
+      Friend David, I do not think we shall need a call bell as Hello! can be heard 10 to 20 feet away. What you think? Edison – P.S. first cost of sender & receiver to manufacture is only $7.00.[12]
+
+      The first name tags to include Hello may have 1880 at Niagara Falls, which was the site of the first telephone operators convention. By 1889, central telephone exchange operators were known as "hello-girls" because of the association between the greeting and the telephone.[14][15]
+
+      A 1918 novel uses the spelling "Halloa" in the context of telephone conversations.[16]
+
+      Hullo, hallo, and other spellings
+      "Hallo" redirects here. For other uses, see Hallo (disambiguation).
+      Hello might be derived from an older spelling variant, hullo, which the American Merriam-Webster dictionary describes as a "chiefly British variant of hello",[17] and which was originally used as an exclamation to call attention, an expression of surprise, or a greeting. Hullo is found in publications as early as 1803.[18] The word hullo is still in use, with the meaning hello.[19][20][21][22]
+
+      Hello is alternatively thought to come from the word hallo (1840) via hollo (also holla, holloa, halloo, halloa).[23] The definition of hollo is to shout or an exclamation originally shouted in a hunt when the quarry was spotted:[24][25]
+
+      If I fly, Marcius,/Halloo me like a hare.
+
+      — Coriolanus (I.viii.7), William Shakespeare
+      Fowler's has it that "hallo" is first recorded "as a shout to call attention" in 1864.[26] It is used by Samuel Taylor Coleridge's famous poem The Rime of the Ancient Mariner written in 1798:
+
+      And the good south wind still blew behind,
+      But no sweet bird did follow,
+      Nor any day for food or play
+
+      Came to the mariners' hollo!
+
+      In many Germanic languages, including German, Danish, Norwegian, Dutch and Afrikaans, "hallo" directly translates into English as "hello". In the case of Dutch, it was used as early as 1797 in a letter from Willem Bilderdijk to his sister-in-law as a remark of astonishment.[27]
+
+      Webster's dictionary from 1913 traces the etymology of holloa to the Old English halow and suggests: "Perhaps from ah + lo; compare Anglo Saxon ealā".
+
+      According to the American Heritage Dictionary, hallo is a modification of the obsolete holla (stop!), perhaps from Old French hola (ho, ho! + la, there, from Latin illac, that way).[28]
+
+      "Hello, World" computer program
+      Main article: "Hello, World!" program
+      Students learning a new computer programming language will often begin by writing a "Hello, World!" program, which does nothing but issue the message "Hello, World!" to the user (such as by displaying it on a screen). It has been used since the earliest programs, and in many computer languages. This tradition was further popularised after being printed in an introductory chapter of the book The C Programming Language by Kernighan & Ritchie.[29] The book had reused an example taken from a 1974 memo by Brian Kernighan at Bell Laboratories.[30]
+
+      Before the telephone, verbal greetings often involved a time of day, such as "good morning". When the telephone began connecting people in different time zones, greetings without time gained popularity.[11]
+
+      Thomas Edison is credited with popularizing hullo as a telephone greeting. In previous decades, hullo had been used as an exclamation of surprise (used early on by Charles Dickens in 1850)[12] and halloo was shouted at ferry boat operators by people who wanted to catch a ride.[13] According to one account, halloo was the first word Edison yelled into his strip phonograph when he discovered recorded sound in 1877.[12] Shortly after Alexander Graham Bell invented the telephone, he answered calls by saying "ahoy ahoy", borrowing the term used on ships.[13][14] There is no evidence the greeting caught on.[13] Edison suggested Hello! on August 15, 1877 in a letter to the president of Pittsburgh's Central District and Printing Telegraph Company, T. B. A. David:
+
+      Friend David, I do not think we shall need a call bell as Hello! can be heard 10 to 20 feet away. What you think? Edison – P.S. first cost of sender & receiver to manufacture is only $7.00.[12]
+
+      The first name tags to include Hello may have 1880 at Niagara Falls, which was the site of the first telephone operators convention. By 1889, central telephone exchange operators were known as "hello-girls" because of the association between the greeting and the telephone.[14][15]
+
+      A 1918 novel uses the spelling "Halloa" in the context of telephone conversations.[16]
+
+      Hullo, hallo, and other spellings
+      "Hallo" redirects here. For other uses, see Hallo (disambiguation).
+      Hello might be derived from an older spelling variant, hullo, which the American Merriam-Webster dictionary describes as a "chiefly British variant of hello",[17] and which was originally used as an exclamation to call attention, an expression of surprise, or a greeting. Hullo is found in publications as early as 1803.[18] The word hullo is still in use, with the meaning hello.[19][20][21][22]
+
+      Hello is alternatively thought to come from the word hallo (1840) via hollo (also holla, holloa, halloo, halloa).[23] The definition of hollo is to shout or an exclamation originally shouted in a hunt when the quarry was spotted:[24][25]
+
+      If I fly, Marcius,/Halloo me like a hare.
+
+      — Coriolanus (I.viii.7), William Shakespeare
+      Fowler's has it that "hallo" is first recorded "as a shout to call attention" in 1864.[26] It is used by Samuel Taylor Coleridge's famous poem The Rime of the Ancient Mariner written in 1798:
+
+      And the good south wind still blew behind,
+      But no sweet bird did follow,
+      Nor any day for food or play
+
+      Came to the mariners' hollo!
+
+      In many Germanic languages, including German, Danish, Norwegian, Dutch and Afrikaans, "hallo" directly translates into English as "hello". In the case of Dutch, it was used as early as 1797 in a letter from Willem Bilderdijk to his sister-in-law as a remark of astonishment.[27]
+
+      Webster's dictionary from 1913 traces the etymology of holloa to the Old English halow and suggests: "Perhaps from ah + lo; compare Anglo Saxon ealā".
+
+      According to the American Heritage Dictionary, hallo is a modification of the obsolete holla (stop!), perhaps from Old French hola (ho, ho! + la, there, from Latin illac, that way).[28]
+
+      "Hello, World" computer program
+      Main article: "Hello, World!" program
+      Students learning a new computer programming language will often begin by writing a "Hello, World!" program, which does nothing but issue the message "Hello, World!" to the user (such as by displaying it on a screen). It has been used since the earliest programs, and in many computer languages. This tradition was further popularised after being printed in an introductory chapter of the book The C Programming Language by Kernighan & Ritchie.[29] The book had reused an example taken from a 1974 memo by Brian Kernighan at Bell Laboratories.[30]
     </main>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,71 @@
 import React from 'react'
 import Link from 'next/link';
+import { Button } from "@/components/ui/button"
+import { HamIcon } from 'lucide-react';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
+
 
 const Navbar = () => {
-  return (
-    <nav className="bg-gray-800 p-4">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <div className="text-white text-lg font-bold">
-          Chaitanya / Blog
-        </div>
-        <div className="hidden md:flex space-x-4">
-          <Link href="/" className="text-gray-300 hover:text-white">Home</Link>
-          <Link href="/about" className="text-gray-300 hover:text-white">About</Link>
-          <Link href="/contact" className="text-gray-300 hover:text-white">Contact</Link>
-        </div>
-        <div className="md:hidden">
-          <button className="text-white focus:outline-none">
-            {/* Add a hamburger icon here if desired */}
-            ☰
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="p-4 bg-background/50 sticky top-0 backdrop-blur">
+            <div className="container mx-auto flex justify-between border-b pb-6 items-center">
+                <Link href={"/"}><div className="text-lg font-bold">
+                    Chaitanya / Blog
+                </div></Link>
+
+                <div className="hidden md:flex space-x-4 items-center">
+                    <Link href="/" >Home</Link>
+                    <Link href="/about" >About</Link>
+                    <Link href="/blog" >Blog</Link>
+                    <Link href="/contact" >Contact</Link>
+                    <div>
+                        <Button variant="outline" className="mx-1">Login</Button>
+                        <Button variant="outline" className="mx-1">Signup</Button>
+                    </div>
+                </div>
+
+                <div className="md:hidden">
+                    <Sheet>
+                        <SheetTrigger>
+                            <button className="focus:outline-none">
+                                {/* Add a hamburger icon here if desired */}
+                                {/* ☰ */}
+                                <svg className='w-6 h-6' fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-76h7"></path>
+                                </svg>
+                            </button>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <SheetHeader>
+                                <SheetTitle className="font-bold my-4">Chaitanya | Blog</SheetTitle>
+                                <SheetDescription>
+                                    <div className="flex flex-col gap-6 ">
+                                        <Link href="/" >Home</Link>
+                                        <Link href="/about" >About</Link>
+                                        <Link href="/blog" >Blog</Link>
+                                        <Link href="/contact" >Contact</Link>
+                                        <div>
+                                            <Button variant="outline" className="mx-1">Login</Button>
+                                            <Button variant="outline" className="mx-1">Signup</Button>
+                                        </div>
+                                    </div>
+                                </SheetDescription>
+                            </SheetHeader>
+                        </SheetContent>
+                    </Sheet>
+
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
